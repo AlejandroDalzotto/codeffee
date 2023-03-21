867febb5a857b75c10848e26b3f9a5e2ed78e6b5
AlejandroDalzotto <aledalzotto15@gmail.com>
2023-03-21 08:36:17 -0300

    about agregado

--- README.md
+++ README.md
@@ -9,8 +9,8 @@ Se trata de un sitio web de una cafetería ficticia que permite a los usuarios c
 [![HTML](https://img.shields.io/badge/Html-E34F26?style=for-the-badge&logo=html5&logoColor=white&labelColor=101010)]()</br>
 [![CSS](https://img.shields.io/badge/Css-1572B6?style=for-the-badge&logo=css3&logoColor=white&labelColor=101010)]()</br>
 [![TailwindCSS](https://img.shields.io/badge/Tailwindcss-06B6D4?style=for-the-badge&logo=tailwindcss&logoColor=white&labelColor=101010)]()</br>
-[![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=white&labelColor=101010)]()</br>
-[![NextJS](https://img.shields.io/badge/NextJS-000000?style=for-the-badge&logo=nextdotjs&logoColor=white&labelColor=101010)]()</br>
+[![TypeScript](https://img.shields.io/badge/TypeScript-3178c6?style=for-the-badge&logo=typescript&logoColor=white&labelColor=101010)]()</br>
+[![NextJS](https://img.shields.io/badge/NextJS-505050?style=for-the-badge&logo=nextdotjs&logoColor=white&labelColor=101010)]()</br>
 [![Vercel](https://img.shields.io/badge/Vercel-000000?style=for-the-badge&logo=vercel&logoColor=white&labelColor=101010)]()</br>
 
 ## Librerías | Recursos
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,75 @@
+import Image from "next/image";
+
 export default function AboutPage() {
     return (
-        <div>Aun en progreso</div>
+        <section className="relative bg-gradient-to-tr from-[#ea1059cc] to-[#ff9472cc]">
+            <Image src="/imgs/background_1.png" alt="background" width={1920} height={1080} className="hidden lg:block fixed w-full h-auto -z-10 opacity-20 -translate-y-20" />
+            <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+                <div className="text-center">
+                    <h2 className="text-3xl font-extrabold text-gray-900 sm:text-4xl">
+                        Sobre nosotros
+                    </h2>
+                    <p className="mt-4 text-lg text-neutral-50">
+                        En Codeffee, nos apasiona el café y la programación. Ofrecemos un
+                        ambiente acogedor y relajado para que puedas disfrutar de una buena
+                        taza de café mientras trabajas en tus proyectos.
+                    </p>
+                </div>
+                <div className="mt-20 w-full lg:flex lg:items-center lg:justify-between lg:gap-20">
+                    <div className="lg:w-2/3 my-10 lg:m-0">
+                        <h3 className="text-xl font-bold text-gray-900 mb-4">
+                            Nuestro café
+                        </h3>
+                        <p className="text-lg text-neutral-50">
+                            En Codeffee, creemos que el café es la clave para la productividad y la
+                            creatividad. Utilizamos granos de café de alta calidad, y nuestros
+                            baristas altamente capacitados se aseguran de que cada taza sea
+                            perfecta.
+                        </p>
+                    </div>
+                    <Image
+                        className="w-full lg:w-1/3 h-auto rounded-lg shadow transition-all hover:ring-2 hover:ring-neutral-50 hover:shadow-xl"
+                        src="/imgs/wallpapers/wallpaper_1.jpg"
+                        alt="Imagen"
+                        width={1920}
+                        height={1080}
+                    />
+                </div>
+                <div className="mt-20">
+
+                    <h3 className="text-xl font-bold text-gray-900 mb-4">
+                        Nuestro menú
+                    </h3>
+                    <p className="text-lg text-neutral-50">
+                        En nuestra cafetería, no solo nos enfocamos en el café. También
+                        ofrecemos una amplia selección de deliciosos bocadillos y postres para
+                        complementar tu bebida. Nuestro menú está diseñado para satisfacer
+                        todas las necesidades dietéticas.
+                    </p>
+
+                </div>
+                <div className="mt-20 w-full lg:flex lg:flex-row-reverse lg:items-center lg:justify-between lg:gap-20">
+                    <div className="lg:w-2/3 my-10 lg:m-0">
+                        <h3 className="text-xl font-bold text-gray-900 mb-4">
+                            Nuestro ambiente
+                        </h3>
+                        <p className="text-lg text-neutral-50">
+                            En nuestra cafetería, nos enorgullece crear un ambiente inclusivo y
+                            acogedor para todos nuestros clientes. Desde estudiantes y freelancers
+                            hasta empresarios y amantes del café, siempre encontrarás un lugar en
+                            Codeffee.
+                        </p>
+                    </div>
+                    <Image
+                        className="w-full lg:w-1/3 h-auto rounded-lg shadow transition-all hover:ring-2 hover:ring-neutral-50 hover:shadow-xl"
+                        src="/imgs/wallpapers/wallpaper_3.jpg"
+                        alt="Imagen de la cafetería"
+                        width={1920}
+                        height={1080}
+                    />
+                </div>
+
+            </div>
+        </section>
     )
 }
